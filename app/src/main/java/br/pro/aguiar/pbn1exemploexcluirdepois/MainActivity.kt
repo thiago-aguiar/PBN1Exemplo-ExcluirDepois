e0e14647ed0e47ad39f1140179990a380650558c
Prof. Thiago Vieira de Aguiar <56936784+thiago-aguiar@users.noreply.github.com>
2020-10-23 19:21:08 -0300

    Update MainActivity.kt
    MainActivity: Nova Variável de teste.

--- app/src/main/java/br/pro/aguiar/pbn1exemploexcluirdepois/MainActivity.kt
+++ app/src/main/java/br/pro/aguiar/pbn1exemploexcluirdepois/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    
+    var variavel: String "Este arquivo foi modificado."
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
