a8013a0eeb2b2a89408b69ce2688e9ce8994e06c
Thiago Vieira de Aguiar <thiagoaguiar@tic.ufrj.br>
2020-10-23 19:22:39 -0300

    Atribuição na variável

--- app/src/main/java/br/pro/aguiar/pbn1exemploexcluirdepois/MainActivity.kt
+++ app/src/main/java/br/pro/aguiar/pbn1exemploexcluirdepois/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     
-    var variavel: String "Este arquivo foi modificado."
+    var variavel: String = "Este arquivo foi modificado."
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
